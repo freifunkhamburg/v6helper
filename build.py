4babb07e94287196f3609f3c77af372d1f313e25
kpcyrd <git@rxv.cc>
2015-02-02 23:30:51 +0000

    Push code

---
+++ build.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from glob import glob
+import json
+import os
+
+DOMAIN = 'ff-hh.net'
+OUT = '/etc/nginx/v6helper.d/'
+
+template = '''
+server {
+    listen 80;
+    listen [::]:80;
+
+    server_name %%s.%s;
+
+    access_log off;
+    error_log /dev/null crit;
+
+    location / {
+        proxy_pass http://[%%s];
+        include /etc/nginx/proxy_params;
+    }
+}
+''' % DOMAIN
+
+for path in glob('db/*'):
+    with open(path) as f:
+        x = json.load(f)
+    with open(os.path.join(OUT, x['name']), 'w') as f:
+        pass
+        f.write(template % (x['name'], x['ipv6']))
---
+++ server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, request, redirect
+import socket
+import json
+import os
+import re
+app = Flask(__name__)
+
+DOMAIN = 'ff-hh.net'
+ABUSE = 'rm-pls@hamburg.freifunk.net'
+DB = 'db/'
+
+kwargs = dict(domain=DOMAIN, abuse=ABUSE)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', **kwargs)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    ipv6 = request.form['ipv6']
+
+    if not re.match('^[a-z0-9]+$', name):
+        return 'name rejected'
+
+    with open('blacklist.txt') as f:
+        blacklist = [x.strip() for x in f if x.strip()]
+
+    if name in blacklist:
+        return 'name is blacklisted'
+
+    if os.path.exists(os.path.join(DB, name)):
+        return 'name exists'
+
+    try:
+        socket.inet_pton(socket.AF_INET6, ipv6)
+    except socket.error:
+        return 'invalid ipv6'
+    else:
+        with open(os.path.join(DB, name), 'w') as f:
+            json.dump({'name': name, 'ipv6': ipv6}, f)
+
+        return redirect('/done')
+
+
+@app.route('/done')
+def done():
+    return render_template('done.html', **kwargs)
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=5001)
